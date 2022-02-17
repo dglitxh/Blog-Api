2d691a6ed867907b94affb97698fcf488c3d17ba
YD <ydglitch@gmail.com>
2022-02-17 21:05:59 +0000

    minor-tweaks

--- blog_api/blog_api/settings.py
+++ blog_api/blog_api/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from .secrets import secret_key
 import os
 import django_on_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,7 +118,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    }, 
+    
 }
 
 
@@ -164,3 +166,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Activate Django-Heroku.
 django_on_heroku.settings(locals())
+
+# production database
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
