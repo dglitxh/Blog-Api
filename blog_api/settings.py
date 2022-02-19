89a6f44c49c0af794e553ae795c5bc8f19ed2450
YD <ydglitch@gmail.com>
2022-02-19 04:38:27 +0000

    settings

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-i2@_oa759z7%emw_-4_4v1uel@%$6dhkok5osp*zfq3bk!h8q=')
+SECRET_KEY = SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
